78c1ddae5ba3031ca8537aa3dfdb0de42ddb7e5f
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-20 09:23:25 +0530

    adding jsconfig file

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+
 import "dotenv/config";
-import express from "express";
+import Fastify from "fastify";
 import { connectDB } from "./src/config/connect.js";
 
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
-  const app = express();
+
+  const app = Fastify();
   const PORT = process.env.PORT || 3000;
 
-  app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+  app.listen({ port:PORT, host: "0.0.0.0" }, (err, addr) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(`Blinkit server listening on ${PORT}`);
+      console.log(`Blinkit server on http://localhost:${PORT}`);
     }
   });
 };
