7f8f7dbcbc51637f3a3be29bbfde28d9fc2dadd5
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-20 09:46:46 +0530

    setupjs file but not complete till now

--- src/config/setup.js
+++ src/config/setup.js
@@ -1,3 +1,53 @@
 import AdminJS from "adminjs";
+import AdminJSFastify from "@adminjs/fastify";
+import * as AdminJSMongoose from "@adminjs/mongoose";
+import * as Models from "../models/index.js";
 
-AdminJS
+AdminJS.registerAdapter(AdminJSMongoose);
+
+export const admin = new AdminJS({
+  resources: [
+    {
+      resource: Models.Customer,
+      options: {
+        listProperties: ["phone", "role", "isActivated"],
+        filterProperties: ["phone", "role"],
+      },
+    },
+    {
+      resource: Models.DeliveryPartner,
+      options: {
+        listProperties: ["email", "role", "isActivated"],
+        filterProperties: ["email", "role"],
+      },
+    },
+    {
+      resource: Models.Admin,
+      options: {
+        listProperties: ["email", "role", "isActivated"],
+        filterProperties: ["email", "role"],
+      },
+    },
+    {
+      resource: Models.Branch,
+    },
+  ],
+  branding: {
+    companyName: "Blinkit",
+    withMadeWithLove: false,
+  },
+  rootPath: "/admin",
+});
+
+export const buildAminRouter = async(app) => {
+    await AdminJSFastify.builAuthenticatedRouter(
+        admin,
+        {
+
+        },
+        app,
+        {
+            store: sessionStore
+        }
+    )
+}
